059efcb4d78140c108f4d5a1fa55bef5688ae89d
Nazar <n.suhovich@gmail.com>
2016-05-10 14:13:22 +0300

    Add user22 product flavour.

---
+++ app/src/user_22/java/com/golfapp/test/AppConstants.java
@@ -0,0 +1,22 @@
+package com.golfapp.test;
+
+/**
+ * Created by Fedir on 30.04.2016.
+ */
+public class AppConstants {
+
+    public static  int[] course_1 = {R.raw.course_1_hole_1, R.raw.course_1_hole_2, R.raw.course_1_hole_3,
+            R.raw.course_1_hole_4, R.raw.course_1_hole_5, R.raw.course_1_hole_6, R.raw.course_1_hole_7,
+            R.raw.course_1_hole_8, R.raw.course_1_hole_9, R.raw.course_1_hole_10, R.raw.course_1_hole_11, R.raw.course_1_hole_12,
+            R.raw.course_1_hole_13, R.raw.course_1_hole_14, R.raw.course_1_hole_15, R.raw.course_1_hole_16, R.raw.course_1_hole_17, R.raw.course_1_hole_18};
+
+    public static int [] course_2 = {R.raw.course_2_hole_1, R.raw.course_2_hole_2, R.raw.course_2_hole_3,
+            R.raw.course_2_hole_4, R.raw.course_2_hole_5, R.raw.course_2_hole_6, R.raw.course_2_hole_7,
+            R.raw.course_2_hole_8, R.raw.course_2_hole_9};
+
+    public static int [] course_3 = {R.raw.course_3_hole_1, R.raw.course_3_hole_2, R.raw.course_3_hole_3};
+
+
+
+    public static int currentCourse = 0;
+}
--- app/src/user_22/java/com/golfapp/test/utils/Constants.java
+++ app/src/user_22/java/com/golfapp/test/utils/Constants.java
@@ -12,7 +12,7 @@ import java.util.Locale;
  */
 public class Constants {
 
-    public static final String app_name = "Golf App DE";
+    public static final String app_name = "Golf App";
     public static final String clientId = "22";
 
     public static int draw = 7;
