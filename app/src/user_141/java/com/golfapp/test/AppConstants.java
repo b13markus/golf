3dd2afebffae66213ba4d6342c4b1b9f4555fd5d
b13markus <xeadxead>
2016-05-31 17:21:30 +0300

    delete course 2 from user 141

--- app/src/user_141/java/com/golfapp/test/AppConstants.java
+++ app/src/user_141/java/com/golfapp/test/AppConstants.java
@@ -10,9 +10,7 @@ public class AppConstants {
             R.raw.course_1_hole_8, R.raw.course_1_hole_9, R.raw.course_1_hole_10, R.raw.course_1_hole_11, R.raw.course_1_hole_12,
             R.raw.course_1_hole_13, R.raw.course_1_hole_14, R.raw.course_1_hole_15, R.raw.course_1_hole_16, R.raw.course_1_hole_17, R.raw.course_1_hole_18};
 
-    public static int [] course_2 = {R.raw.course_2_hole_1, R.raw.course_2_hole_2, R.raw.course_2_hole_3,
-            R.raw.course_2_hole_4, R.raw.course_2_hole_5, R.raw.course_2_hole_6, R.raw.course_2_hole_7,
-            R.raw.course_2_hole_8, R.raw.course_2_hole_9};
+    public static int [] course_2 = {};
 
     public static int [] course_3 = {};
 
