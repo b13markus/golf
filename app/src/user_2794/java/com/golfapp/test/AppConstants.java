06a9d0885eeae946083a363eb596912586b5b92c
b13markus <xeadxead>
2016-06-08 11:17:10 +0300

    updated images

--- app/src/user_2794/java/com/golfapp/test/AppConstants.java
+++ app/src/user_2794/java/com/golfapp/test/AppConstants.java
@@ -5,9 +5,9 @@ package com.golfapp.test;
  */
 public class AppConstants {
 
-    public static  int[] course_1 = {R.raw.course_1_hole_1, R.raw.course_1_hole_2, R.raw.course_1_hole_3,
-            R.raw.course_1_hole_4, R.raw.course_1_hole_5, R.raw.course_1_hole_6, R.raw.course_1_hole_7,
-            R.raw.course_1_hole_8, R.raw.course_1_hole_9, R.raw.course_1_hole_10, R.raw.course_1_hole_11, R.raw.course_1_hole_12,
+    public static  int[] course_1 = {R.raw.course_1_hole_01, R.raw.course_1_hole_02, R.raw.course_1_hole_03,
+            R.raw.course_1_hole_04, R.raw.course_1_hole_05, R.raw.course_1_hole_06, R.raw.course_1_hole_07,
+            R.raw.course_1_hole_08, R.raw.course_1_hole_09, R.raw.course_1_hole_10, R.raw.course_1_hole_11, R.raw.course_1_hole_12,
             R.raw.course_1_hole_13, R.raw.course_1_hole_14, R.raw.course_1_hole_15, R.raw.course_1_hole_16, R.raw.course_1_hole_17, R.raw.course_1_hole_18};
 
     public static int [] course_2 = {};
